775b41d14324e63abd6de7ce1835a29970026c3f
Siggysic <siggysic@gmail.com>
2016-07-24 22:51:03 +0700

    contact

---
+++ application/contact-us.php
@@ -0,0 +1,90 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../lib/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../lib/css/custom.css">
+    <script src="../lib/js/jquery-1.12.4.min.js"></script>
+    <script src="../lib/js/bootstrap.min.js"></script>
+
+  </head>
+
+  <body>
+    <div class="row">
+      <div class="container">
+        <!-- <img src="img/kmutnb.png" class="img-responsive img-logo"> -->
+        <h3 class="text-center">ระบบจัดการตารางสอบ</h3>
+        <h4 class="text-right">คณะครุศาสตร์อุตสาหกรรม</h4>
+      </div>
+    </div>
+
+    <div class="row">
+      <nav class="navbar navbar-default">
+        <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Brand</a>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#"><i class="glyphicon glyphicon-home padding-right"></i>หน้าหลัก</a></li>
+              <li><a href="#"><i class="glyphicon glyphicon-th-list padding-right"></i>จัดตารางสอบ</a></li>
+              <li><a href="#"><i class="glyphicon glyphicon-search padding-right"></i>ตรวจสอบตารางสอบ</a></li>
+              <li><a href="#">เปลี่ยนรหัสผ่าน</a></li>
+              <li><a href="#">ติดต่อเรา</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
+    </div>
+    
+    <div class="row">
+      <div class="container">
+        <div class="col-sm-2">
+          <a href="#sidebar" data-toggle="collapse"><i class="glyphicon glyphicon-align-justify"></i></a>
+          <ul id="sidebar" class="nav nav-pills nav-stacked panel-collapse collapse">
+            <li><a href="#">ข้อมูลวิชาสอบ</a></li>
+            <li><a href="#">ข้อมูลห้องสอบ</a></li>
+            <li><a href="#">ข้อมูลผู้สอบ</a></li>
+            <li><a href="#">จัดห้องสอบอัตโนมัติ</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-10">
+          <div class="container-fluid">
+            <div class="panel panel-primary">
+              <div class="panel-heading"><i class="glyphicon glyphicon-user padding-right"></i>ติดต่อเรา</div>
+              <div class="panel-body">
+                <div class="row">
+                  <div class="container-fluid text-center">
+                    <img src="img/kmutnb.png" width="200px" height="200px">
+                  </div>
+                </div><br>
+                <div class="row">
+                  <div class="container-fluid text-center">
+                    <h4><strong>ชื่อ : </strong>เพรชกำแพงยิง มุ้งมิ้งมากมาย</h4>
+                    <h4><strong>Email : </strong>Petchgumpangying@gmail.com</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <nav class="navbar navbar-default navbar-fixed-bottom footer">
+      <div class="container">
+        <h5 class="text-center">คณะครุฑศาสตร์อุตสาหกรรม มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ</h5>
+      </div>
+    </nav>
+
+  </body>
+
+</html>
